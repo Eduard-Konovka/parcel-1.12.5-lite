6d52e1ecfb258d1f1b9ba3f2bc6a824eff3272db
Eduard-Konovka <ed098ua@gmail.com>
2021-10-04 17:13:24 +0300

    Изменяет способ подключения стилей

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 // --------------------------------- ИМПОРТЫ ----------------------------------
 // ----------------------------------------------------------------------------
 
+import './sass/styles.css'
 import db from './db/db.json'
 import templ from './templates/templ.hbs'
 import refs from './js/refs'
